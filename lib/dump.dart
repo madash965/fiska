7417bc4b14d26823cbe080f8310e3827d50457ac
madash965 <madash965@gmail.com>
2021-12-25 11:38:49 -0800

    added

---
+++ lib/dump.dart
@@ -0,0 +1,122 @@
+// List<Product> products = [
+//   Product(
+//     image: "assets/product1.jpg",
+//     description: "This is dummy data for Fiska eCommerce application",
+//     price: "900",
+//     productName: "iPad mini",
+//   ),
+//   Product(
+//     image: "assets/product2.jpg",
+//     description: "This is dummy data for Fiska eCommerce application.",
+//     price: "700",
+//     productName: "iPad Pro",
+//   ),
+//   Product(
+//     image: "assets/product3.jpg",
+//     description: "This is dummy data for Fiska eCommerce application",
+//     price: "800",
+//     productName: "iPhone Pro Max",
+//   ),
+//   Product(
+//     image: "assets/product4.jpg",
+//     description: "This is dummy data for Fiska eCommerce application.",
+//     price: "90",
+//     productName: "Apple Watch Series 3",
+//   ),
+//   Product(
+//     image: "assets/product5.jpg",
+//     description: "This is dummy data for Fiska eCommerce application.",
+//     price: "500",
+//     productName: "Apple Watch Series 4",
+//   ),
+//   Product(
+//     image: "assets/product6.jpg",
+//     description: "This is dummy data for Fiska eCommerce application.",
+//     price: "100",
+//     productName: "Macbook Pro 16 inch",
+//   ),
+//   Product(
+//     image: "assets/product7.jpg",
+//     description: "This is dummy data for Fiska eCommerce application.",
+//     price: "200",
+//     productName: "Macbook Pro",
+//   ),
+//   Product(
+//     image: "assets/product8.jpg",
+//     description: "This is dummy data for Fiska eCommerce application",
+//     price: "1000",
+//     productName: "iMac 4k Retina",
+//   ),
+//   Product(
+//     image: "assets/product9.jpg",
+//     description: "This is dummy data for Fiska eCommerce application",
+//     price: "150",
+//     productName: "T-Shirts",
+//   ),
+//   Product(
+//       image: "assets/product10.jpg",
+//       description: "This is dummy data for Fiska eCommerce application",
+//       price: "100",
+//       productName: "Ethnic Wear - Dress"),
+//   Product(
+//       image: "assets/product11.jpg",
+//       description: "This is dummy data for Fiska eCommerce application.",
+//       price: "100",
+//       productName: "Dress"),
+//   Product(
+//       image: "assets/product12.jpg",
+//       description: "This is dummy data for Fiska eCommerce application.",
+//       price: "100",
+//       productName: "T-Shirt"),
+// ];
+
+
+//Container(
+            //   decoration: BoxDecoration(
+            //     color: Colors.white,
+            //     borderRadius: BorderRadius.circular(10),
+            //   ),
+            //   child: Center(
+            //     child: Row(
+            //       children: [
+            //         Text(
+            //           "Filter: ",
+            //           style: TextStyle(
+            //             fontWeight: FontWeight.w800,
+            //             fontSize: 20,
+            //             color: Colors.black,
+            //           ),
+            //         ),
+            //         Container(
+            //           decoration: BoxDecoration(
+            //             color: Colors.orange[300],
+            //             borderRadius: BorderRadius.circular(10),
+            //           ),
+            //           padding: EdgeInsets.all(8),
+            //           child: DropdownButtonHideUnderline(
+            //             child: DropdownButton(
+            //               value: _choice,
+            //               onChanged: (value) {
+            //                 _choice = value;
+            //               },
+            //               items: status
+            //                   .map(
+            //                     (value) => DropdownMenuItem(
+            //                       child: Text(
+            //                         value,
+            //                         style: TextStyle(
+            //                           fontWeight: FontWeight.w600,
+            //                           fontSize: 16,
+            //                           color: Colors.black,
+            //                         ),
+            //                       ),
+            //                     ),
+            //                   )
+            //                   .toList(),
+            //             ),
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
